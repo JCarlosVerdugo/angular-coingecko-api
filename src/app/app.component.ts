192b99ec8b775875b1d2affcf7ec6af9174d4d9b
JCarlosVerdugo <carlosverdugo@proton.me>
2023-07-16 22:27:27 -0700

    Getting data from CoinGecko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-coingecko-api';
+export class AppComponent implements OnInit  {
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+
+  ngOnInit() {
+    this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+      .subscribe({
+        next: (res) => {
+          console.log(res)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+  }
+
+
 }
