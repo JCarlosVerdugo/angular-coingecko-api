9049e7f2146890ae2d39c0c2ce4481ef613ecf67
JCarlosVerdugo <carlosverdugo@proton.me>
2023-07-27 21:51:45 -0700

    Show data in table using coincap API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+interface Coin{
+  exchangeId:            string;
+  rank:                  string;
+  baseSymbol:            string;
+  baseId:                string;
+  quoteSymbol:           string;
+  quoteId:               string;
+  priceQuote:            string;
+  priceUsd:              string;
+  volumeUsd24Hr:         string;
+  percentExchangeVolume: string;
+  tradesCount24Hr:       string;
+  updated:               number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit  {
+  coins: Coin[] = [];
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    '24h Volume',
+  ];
 
   constructor(
     private http: HttpClient
   ) {}
 
-
   ngOnInit() {
-    this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    // this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin')
+    this.http.get<Coin[]>('https://api.coincap.io/v2/markets')
       .subscribe({
-        next: (res) => {
-          console.log(res)
+        next: (res: any) => {
+          this.coins = res.data;
         },
         error: (err) => {
           console.log(err)
